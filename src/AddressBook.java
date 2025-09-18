71dea476033cd8e649647798df21f1a08cd265c9
EnderAres <love2fish183@gmail.com>
2025-09-18 10:25:54 -0400

    feat: add print statement

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -22,6 +22,7 @@ public class AddressBook {
         AddressBook addressBook = new AddressBook();
         addressBook.addBuddy(buddy);
         addressBook.removeBuddy(buddy);
+        System.out.println("hello");
     }
 
 }
